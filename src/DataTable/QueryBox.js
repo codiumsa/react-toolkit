e141c36f624b3b889c80e308e388e914a26c0c13
Jorge Ramírez <jorgeramirez1990@gmail.com>
2017-11-04 21:17:42 -0300

    Se actualiza QueryBox
    Signed-off-by: Jorge Ramírez <jorgeramirez1990@gmail.com>

--- src/DataTable/QueryBox.js
+++ src/DataTable/QueryBox.js
@@ -1,39 +1,38 @@
 import React from 'react';
-import { withStyles } from 'material-ui/styles';
+import {withStyles} from 'material-ui/styles';
 import Toolbar from 'material-ui/Toolbar';
 import Input from 'material-ui/Input';
-import { FormControl } from 'material-ui/Form';
+import {FormControl} from 'material-ui/Form';
 import Chip from 'material-ui/Chip';
 import Typography from 'material-ui/Typography';
 import Avatar from 'material-ui/Avatar';
 import IconButton from 'material-ui/IconButton';
 import SearchIcon from 'material-ui-icons/Search';
-
+import PropTypes from 'prop-types';
 
 const styleSheet = theme => ({
-    underline: {
-        '&:before': {
-            backgroundColor: 'rgba(0, 0, 0, 0.1)',
-
-        }
+  underline: {
+    '&:before': {
+      backgroundColor: 'rgba(0, 0, 0, 0.1)',
     },
-    chip: {
-        backgroundColor: theme.palette.primary['50']
-    },
-    avatar: {
-        backgroundColor: theme.palette.primary['200']
-    },
-    row: {
-        display: 'flex',
-        flexWrap: 'wrap',
-    },
-    filtersContainer: {
-        paddingLeft: theme.spacing.unit * 3,
-        paddingRight: theme.spacing.unit * 3
-    },
-    filterLabel: {
-        color: 'rgba(0, 0, 0, 0.54)'
-    }
+  },
+  chip: {
+    backgroundColor: theme.palette.primary['50'],
+  },
+  avatar: {
+    backgroundColor: theme.palette.primary['200'],
+  },
+  row: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  filtersContainer: {
+    paddingLeft: theme.spacing.unit * 3,
+    paddingRight: theme.spacing.unit * 3,
+  },
+  filterLabel: {
+    color: 'rgba(0, 0, 0, 0.54)',
+  },
 });
 
 /**
@@ -45,61 +44,70 @@ const styleSheet = theme => ({
  * }
  * Ademas, recibe los dispatches para:
  *  1. onQueryChanged: value => {} cuando se cambia el valor del campo de busqueda
- *  2. onQueryRequested: () => {} tiene como objetivo ser manejado por 
- *  el reducer de historico de busquedas 
- * @param {* props} props 
+ *  2. onQueryRequested: () => {} tiene como objetivo ser manejado por
+ *  el reducer de historico de busquedas
+ * @param {* props} props
  */
 
-
 const QueryBox = props => {
-    const { classes } = props;
-    return (
-        <div>
-            {props.queryData.generalFilter ?
-                <div className={classes.filtersContainer}>
-                    <Typography type="body2" className={classes.filterLabel}>Filtros aplicados</Typography>
-                    <div className={classes.row}>
-                        <Chip
-                            avatar={<Avatar className={classes.avatar}>=</Avatar>}
-                            label={
-                                <div style={{ display: 'flex', alignItems: 'center' }}>
-                                    <small style={{ fontSize: '0.7em' }}>Filtro general&nbsp;  &nbsp;</small>
-                                    <span>{props.queryData.generalFilter}</span>
-                                </div>
-                            }
-                            onRequestDelete={() => { props.onQueryChanged(''); props.onQueryRequested() }}
-                            classes={{ root: classes.chip }}
-                        />
-                    </div>
+  const {classes} = props;
+  return (
+    <div>
+      {props.queryData.generalFilter
+        ? <div className={classes.filtersContainer}>
+          <Typography type="body2" className={classes.filterLabel}>
+              Filtros aplicados
+          </Typography>
+          <div className={classes.row}>
+            <Chip
+              avatar={<Avatar className={classes.avatar}>=</Avatar>}
+              label={
+                <div style={{display: 'flex', alignItems: 'center'}}>
+                  <small style={{fontSize: '0.7em'}}>
+                      Filtro general&nbsp;  &nbsp;
+                  </small>
+                  <span>{props.queryData.generalFilter}</span>
                 </div>
-                :
-                <Toolbar>
-                    <IconButton disabled>
-                        <SearchIcon />
-                    </IconButton>
-                    <FormControl fullWidth >
-                        <Input
-                            classes={{ underline: classes.underline }}
-                            fullWidth
-                            autoFocus
-                            placeholder="Filtro general"
-                            value={props.queryData.generalFilterInput}
-                            onChange={event => {
-                                props.onQueryChanged(event.target.value);
-                            }}
-                            onKeyDown={
-                                event => {
-                                    if (event.keyCode === 13) {
-                                        props.onQueryRequested();
-                                    }
-                                }
-                            }
-                        />
-                    </FormControl>
-                </Toolbar>
-            }
+              }
+              onRequestDelete={() => {
+                props.onQueryChanged('');
+                props.onQueryRequested();
+              }}
+              classes={{root: classes.chip}}
+            />
+          </div>
         </div>
-    )
-}
+        : <Toolbar>
+          <IconButton disabled>
+            <SearchIcon />
+          </IconButton>
+          <FormControl fullWidth>
+            <Input
+              classes={{underline: classes.underline}}
+              fullWidth
+              autoFocus
+              placeholder="Filtro general"
+              value={props.queryData.generalFilterInput}
+              onChange={event => {
+                props.onQueryChanged(event.target.value);
+              }}
+              onKeyDown={event => {
+                if (event.keyCode === 13) {
+                  props.onQueryRequested();
+                }
+              }}
+            />
+          </FormControl>
+        </Toolbar>}
+    </div>
+  );
+};
+
+QueryBox.propTypes = {
+  classes: PropTypes.any,
+  queryData: PropTypes.any.isRequired,
+  onQueryChanged: PropTypes.func.isRequired,
+  onQueryRequested: PropTypes.func.isRequired,
+};
 
 export default withStyles(styleSheet)(QueryBox);
